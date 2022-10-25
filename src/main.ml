840a36a2120ea52d3f6d0c88f5b3a1572da1bd06
et4te <et4te@protonmail.com>
2022-10-25 21:26:45 +0700

    ocaml-dalek: Added example executable directive in `dune`.

--- src/main.ml
+++ src/main.ml
@@ -1,37 +1,6 @@
 open Binding
-
-let with_opaques f =
-  f (); Gc.full_major ()
-
-let with_arena f =
-  let open Alien_ffi in
-  let arena = Arena.alloc () in
-  f arena;
-  Arena.free arena
-
-let with_full f =
-  with_opaques (fun () -> with_arena (fun arena -> f arena))
-
-let bench f =
-  let t1 = Unix.gettimeofday () in
-  let () = f () in
-  let t2 = Unix.gettimeofday () in
-  t2 -. t1
-
-let bench msg ~items ~prepare run =
-  let runs = 10 in
-  let value = prepare () in
-  let results =
-    List.init runs (fun n ->
-        let time = bench (fun () -> run value) in
-        Format.eprintf "%s(%d): %.3f\n%!" msg n time;
-        time)
-  in
-  let total = List.fold_left (fun acc time -> acc +. time) 0.0 results in
-  let average = total /. Int.to_float runs in
-  let per_second = Int.to_float items /. average in
-  Format.eprintf "%s: %.3f\n%!" msg average;
-  Format.eprintf "%s: %.3f/s\n%!" msg per_second
+open Alien_ffi.Interface
+open Alien_ffi.Bench
 
 let () =
   let items = 1000 in
