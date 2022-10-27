85993d35f6812bdae476b5a12ae315b82653f43e
Ulrik Strid <ulrik.strid@outlook.com>
2022-10-27 14:45:39 +0200

    Fix nix typo

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
       in rec {
         packages = {
           libdalek_rs = code.libdalek_rs;
-          ocaml-dalek = code.ocaml_dalek;
+          ocaml-dalek = code.ocaml-dalek;
           all = pkgs.symlinkJoin {
             name = "all";
             paths = with code; [ (ocaml-dalek pkgs.ocaml-ng.ocamlPackages_5_0) libdalek_rs ];
